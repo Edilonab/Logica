c0812af0a58f7a92d3bd4c59886ae762264eefbf
Edilonab <90010997+Edilonab@users.noreply.github.com>
2022-05-02 00:04:03 -0300

    Pronto

--- logica.php
+++ logica.php
@@ -1,11 +1,26 @@
 <?php
-    /*Lógica de programação
-    Pensando em todos os números naturais inferiores a 10 que são múltiplos de 3 ou 5,
-    temos 3, 5, 6 e 9. Somando esses múltiplos obtemos o valor 23. 
-    Utilize um algorítimo para calcular a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.*/
+ 
+    function countMultiples($n)
+    {
+        $res = 0;
+        for ($i = 1; $i < $n; $i++)
 
+            if ($i % 3 == 0 || $i % 5 == 0)
+
+                ($res += $i);
     
+        return $res;
+    }
+    
+    echo "Valor é = " ,countMultiples(1000);
+        
 
 
+/*
+Lógica de programação
 
+Pensando em todos os números naturais inferiores a 10 que são múltiplos de 3 ou 5, 
+temos 3, 5, 6 e 9. Somando esses múltiplos obtemos o valor 23. Utilize um algorítimo 
+para calcular a soma de todos os múltiplos de 3 ou 5 abaixo de 1000
+*/ 
 ?>
